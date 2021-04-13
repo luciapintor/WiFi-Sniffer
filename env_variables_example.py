74fd244376528bbd87989820e2123dd6ac686833
lucia <luciapintor90@gmail.com>
2021-04-13 15:04:06 +0200

    First capture version

---
+++ env_variables_example.py
@@ -0,0 +1,4 @@
+# here the variables
+
+channels = [1, 6, 11]
+sniffing_duration = 20
--- start_sniffing.py
+++ start_sniffing.py
@@ -15,8 +15,24 @@ def start_sniffing(monitor_list, sniffing_duration=10):
     if not os.path.exists(data_folder):
         os.makedirs(data_folder)
 
+    # ask mode
+    mode_list = [
+        'A - Active screen (WiFi on and no Power Saving)',
+        'S - Standby (WiFi on and no Power Saving)',
+        'PA - Power Saving with Active screen',
+        'PS - Power Saving with Standby',
+        'WA - WiFi off with Active screen',
+        'WS - WiFi off with Standby',
+    ]
+
+    for mode in mode_list:
+        print(mode)
+
+    device_mode = input('Type the smartphone mode acronym:\n')
+
     # capture timestamp
     cap_ts = datetime.now().strftime("%Y-%b-%d-h%H-m%M-s%S")
+    print("Capture started at {}".format(cap_ts))
 
     # subprocesses list
     cap_sub = []
@@ -24,14 +40,17 @@ def start_sniffing(monitor_list, sniffing_duration=10):
     for m in monitor_list:
         phy_id, mon_id, channel = m
 
-        filename = '{}/{}-ch{}.pcap'.format(data_folder, cap_ts, channel)
+        filename = '{}/{}-ch{}-mode{}.pcap'.format(data_folder, cap_ts, channel, device_mode)
 
         # start the capture in a subprocess
         cap_sub.append(
             subprocess.Popen(['tcpdump',
                               '-i', mon_id,  # Select the interface
                               '-n',  # Don't convert addresses
-                              '-w', filename  # Save output
+                              '-tt',  # Timestamp seconds
+                              '-e',  # Link level header is printed out
+                              'type', 'mgt', 'subtype', 'probe-req',
+                              '-w', filename,  # Save output
                               ], stdout=subprocess.PIPE))
 
     # sleep while sub processes make captures 
